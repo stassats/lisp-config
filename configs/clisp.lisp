9276d08614e2cd546fbc21862b06e86fd8d4ca2f
Stas Boukarev <stassats@gmail.com>
2009-01-19 10:13:25 +0300

    Adjust clisp.

--- configs/clisp.lisp
+++ configs/clisp.lisp
@@ -1,14 +1,1 @@
-
-(unless (find :asdf *features*)
- (load (merge-pathnames "lisp/site/asdf/asdf.lisp"
-			(user-homedir-pathname))))
-
-(setf asdf:*central-registry*
-      '(*default-pathname-defaults*
-	(merge-pathnames "lisp/systems/" (user-homedir-pathname))))
-
-(asdf:operate 'asdf:load-op :asdf-binary-locations)
-
-(setf asdf:*centralize-lisp-binaries* t
-      asdf:*default-toplevel-directory*
-      (merge-pathnames "lisp/fasls/" (user-homedir-pathname)))
+(load (merge-pathnames "lisp/configs/share.lisp" (user-homedir-pathname)))
