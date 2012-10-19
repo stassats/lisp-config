70bc471604867aa6f0d599bb10e8712c170ab34b
Stas Boukarev <stassats@gmail.com>
2012-10-19 05:26:09 +0400

    Better abcl support.

--- configs/abcl.lisp
+++ configs/abcl.lisp
@@ -1,3 +1,9 @@
 ;; -*- Mode: Lisp -*-
 
 (load (merge-pathnames "lisp/configs/share.lisp" (user-homedir-pathname)))
+
+(setf asdf:*central-registry*
+      (append (mapcar #'pathname
+                      (mapcar #'directory-namestring
+                              (directory "~/lisp/impl/abcl/contrib/*/*.asd")))
+              asdf:*central-registry*))
